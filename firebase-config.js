06bca9fcc87f83ca3370a430a859ee88bae48851
Dar-Store <abdelrahmanatefdiab@gmail.com>
2026-06-29 11:08:34 +0300

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDID3MlNnUXF-3PEdyOUqOUih5lnnw8bLU",
+  authDomain: "dar-store-f4a1d.firebaseapp.com",
+  projectId: "dar-store-f4a1d",
+  storageBucket: "dar-store-f4a1d.firebasestorage.app",
+  messagingSenderId: "191261289733",
+  appId: "1:191261289733:web:f396be4ed99e1117f4db6c",
+  measurementId: "G-0JDVXJZV1Q"
+};
